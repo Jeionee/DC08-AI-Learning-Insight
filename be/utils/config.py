e8d9d5c89d2ade02b07905aa3b5fa86533c94144
marcoismakalew <marcoismakalew@gmail.com>
2025-12-07 13:36:03 +0800

    refactor: profile route

--- be/utils/config.py
+++ be/utils/config.py
@@ -1,10 +1,9 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your-secret-key")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)  # 15 minutes expiry
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
